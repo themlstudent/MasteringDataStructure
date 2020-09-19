2ee43d620886a32fa7eb1baa2408444110974edc
Aditya Raj <65248775+themlstudent@users.noreply.github.com>
2020-09-19 22:54:18 +0530

    Add files via upload

---
+++ Mathematics/gcd of two number using Euclidean Algorithm.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int gcd(int a,int b)
+{
+	while(a!=b)
+	{
+		if(a>b)
+		{
+			a=a-b;
+		}
+		else 
+		{
+			b=b-a;
+		}
+	}
+	return a;
+}
+int main()
+{
+	int a = 4, b = 6;
+    	
+    	cout<<gcd(a, b);
+    	
+    	return 0;
+}
