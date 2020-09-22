d34b02f781fa30adcc624d0ed9e165e6f6dbe304
Aditya Raj <65248775+themlstudent@users.noreply.github.com>
2020-09-22 18:42:41 +0530

    Add files via upload

---
+++ Mathematics/All divisors of a given number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void AllDivisors(int n)
+{
+	for(int i=1;i<n;i++)
+	{
+		if(n%i==0)
+		{
+			cout<<i<<endl;
+		}
+	}
+}
+int main()
+{
+	int n=100;
+	AllDivisors(n);
+}
