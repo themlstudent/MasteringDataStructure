ba79dd05a6d38ff7695fe710af058c93089a056c
Aditya Raj <65248775+themlstudent@users.noreply.github.com>
2020-09-29 22:54:08 +0530

    Add files via upload

---
+++ Mathematics/Prime number for large inputs.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+bool prime(int n)
+{
+	if(n==1)
+	{
+		return false;
+	}
+	if(n==2 || n==3)
+	{
+		return false;
+	}
+	if(n%2==0 || n%3==0)
+	{
+		return false;
+	}
+	for(int i=5;i*i<=n;i=i+6)
+	{
+		if(n%i==0||n%(i+2)==0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+int main()
+{
+	int n =10;
+	cout<<(bool)prime(n);
+	return 0;
+}
