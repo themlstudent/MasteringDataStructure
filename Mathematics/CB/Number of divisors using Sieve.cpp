ad5b5a2944ee50bff47f5c6cd767ebaf8c5175c0
Aditya Raj <65248775+themlstudent@users.noreply.github.com>
2020-10-01 22:26:40 +0530

    Add files via upload

---
+++ Mathematics/CB/Number of divisors using Sieve.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int>primeSieve(int *p,int n)
+{
+	p[0]=p[1]=0;
+	p[2]=1;
+	//Lets us Mark all odd numbers as Prime(Initialisation)
+	for(int i=3;i<=n;i+=2)
+	{
+		p[i]=1;
+	}
+	// Sieve Login to mark non prime number as zero
+	// 1.Optimisation :Iterate only over odd numbers
+	for(int i=3;i<=n;i+=2)
+	{
+		if(p[i])
+		{
+		// 2. Optimisation take multiple jump of  2i starting from i*i
+		for(int j=i*i;j<=n;j+=2*i)
+		{
+			p[j]=0;
+		}
+		
+		}
+	}
+	
+	vector <int>primes;
+	primes.push_back(2);
+	for(int i=3;i<=n;i+=2)
+	{
+		if(p[i]==1)
+		{
+			primes.push_back(i);
+		}
+	}
+	return primes;
+}
+int no_of_divisors(int m,vector<int> &primes)
+{
+	
+	int i=0;
+	int p=primes[0];
+	int ans=1;
+	while(p*p<=m)
+	{
+		if(m%p==0)
+		{
+			int cnt = 0 ;
+			
+			while(m%p==0)
+			{ 
+			    cnt++;
+				m=m/p;
+			}
+			ans=ans *(cnt+1);
+		}
+		// go to next position
+		i++;
+		p=primes[i];
+	}
+	if(m!=1)
+	{
+		ans=ans*2;
+	}
+	return ans;
+}
+
+
+int main()
+{
+	int N=1000000;
+	int p[N]={0};
+	vector<int>primes=primeSieve(p,100);
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int no;
+		cin>>no;
+		int divs =no_of_divisors(no,primes);
+		cout<<divs<<endl;
+	}
+	return 0;
+}
