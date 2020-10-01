e0f7295041e5f2d052d722437130f1519cd63df9
Aditya Raj <65248775+themlstudent@users.noreply.github.com>
2020-10-01 22:25:48 +0530

    Add files via upload

---
+++ Mathematics/CB/Prime Factorisation.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std ;
+void printFactors(int n)
+{
+	vector<pair<int,int> >factors;
+	int  cnt;
+	for(int i=2;i*i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			cnt =0;
+			while(n%i==0)
+			{
+				cnt++;
+				n=n/i;
+			}
+			factors.push_back(make_pair(i,cnt));
+		}
+	}
+	if(n!=1)
+	{
+		//whatever is left is a prime number in itself
+		factors.push_back(make_pair(n,1));
+	}
+	for (auto p :factors)
+	{
+		cout<<p.first<<"^"<<p.second<<endl;
+	}
+	return;
+}
+
+int main()
+{
+	int n;
+	cin>> n;
+	
+	printFactors(n);
+	return 0;
+}
