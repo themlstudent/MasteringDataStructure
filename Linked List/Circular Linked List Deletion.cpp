edc4293a2f85e95a45790deb562c612cee5a61cc
Aditya Raj <65248775+themlstudent@users.noreply.github.com>
2021-03-05 19:49:30 +0530

    Add files via upload

---
+++ Linked List/Circular Linked List Deletion.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+using namespace std;
+class node
+{
+	public :
+		int data;
+		node* next;
+		node(int d)
+		{
+			data=d;
+			next =NULL;
+		}
+};
+void insert(node*&head,int data)
+{
+	node*n=new node(data);
+	node*temp=head;
+	n->next=head;
+	if(temp!=NULL)
+	{
+		while(temp->next!=head)
+		{
+			temp=temp->next;
+		}
+		temp->next=n;
+	}
+	else
+	{
+		n->next=n;
+	}
+	head=n;
+}
+void print(node*head)
+{
+	node*temp=head;
+	while(temp->next!=head)
+	{
+		cout<<temp->data<<"->";
+		temp=temp->next;
+	}
+	cout<<temp->data<<endl;
+	return ;
+	
+}
+// Deletion in Linked List:
+node*getnode(node*head,int data)
+{
+node*temp=head;
+//for all the nodes except last node
+while(temp->next!=head)
+{
+	 if(temp->data==data)
+	 {
+	 	return temp;
+	 }
+	 temp=temp->next;
+}
+// out of the loop ,last node
+if(temp->data==data)
+{
+	return temp;
+}
+return NULL;
+	
+}
+void deleteNode(node*&head,int data)
+{
+	node*del=getnode(head,data);
+	if(del==NULL)
+	{
+		return;
+	}
+	// Otherwise
+	if(head==del)
+	{
+		head=head->next;
+	}
+	node*temp=head;
+	// Stop one step behind the node to be deleted
+	while(temp->next!=del)
+	{
+		temp=temp->next;
+	}
+	temp->next=del->next;
+	delete del;
+}
+int main()
+{
+	node*head=NULL;
+	insert(head,10);
+    insert(head,20);
+	insert(head,30);
+	insert(head,40);
+	insert(head,50);
+	print(head);
+	deleteNode(head,30);
+	print(head);
+	return 0;		
+	
+}
