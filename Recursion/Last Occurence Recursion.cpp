ba3ff2e0cc7924cd293c6a65502eda420be635e8
Aditya Raj <65248775+themlstudent@users.noreply.github.com>
2021-04-02 15:48:27 +0530

    Add files via upload

---
+++ Recursion/Last Occurence Recursion.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+int lastOccur(int *a,int n ,int key)
+{
+	if(n==0)
+	{
+		return -1;
+	}
+	// rec case 
+	int i =lastOccur(a+1,n-1,key);
+	if(i==-1)
+	{
+		if(a[0]==key)
+		{
+			return 0;
+		}
+		else
+		{
+			return -1;
+		}
+	}
+	return i+1;
+}
+int firstOccur(int *a,int n,int key)
+{
+	if(n==0)
+	{
+		return -1;
+	}
+	if(a[0]== key)
+	{
+		return 0;
+	}
+	int i=firstOccur(a+1,n-1,key);
+	if(i==-1)
+	{
+		return -1;
+	}
+	return i+1;
+}
+int main()
+{
+	int arr[]={1,2,3,7,4,5,6,7,10};
+	int n=sizeof(arr)/sizeof(int);
+	int key=7;
+	cout<<firstOccur(arr,n,key)<<endl;
+	cout<<lastOccur(arr,n,key);
+	return 0;
+}
