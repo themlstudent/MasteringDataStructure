5d50bcaa68a52c3b8afc56f2bc736e0ef9d7a6d5
Aditya Raj <65248775+themlstudent@users.noreply.github.com>
2021-04-25 12:23:12 +0530

    Add files via upload

---
+++ Linked List/Delete First Node of Doubley Linked List.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+class Node
+{
+     public:
+	 int data;
+	 Node *prev;
+	 Node *next;
+	 Node(int x)
+	 {
+	    data=x;
+		prev=NULL;
+		next=NULL;	
+	 }	
+};
+Node *deleteBeg(Node *head)
+{
+	if(head==NULL)return NULL;
+	if(head->next==NULL)
+	{
+		delete head;
+		return NULL;
+	}
+	else
+	{
+		Node *temp=head;
+		head=head->next;
+		head->prev=NULL;
+		delete temp;
+		return head;
+	}
+	
+}
+void printDLL(Node *head)
+{
+	Node *curr=head;
+	while(curr!=NULL)
+	{
+		cout<<curr->data<<"---->";
+		curr=curr->next;
+	}
+	cout<<endl;
+}
+int main()
+{
+	Node* head=new Node(10);
+	Node*temp1=new Node(20);
+	Node *temp2=new Node(30);
+	head->next=temp1;
+	temp1->prev=head;
+	temp1->next=temp2;
+	temp2->prev=temp1;
+	printDLL(head);
+	head=deleteBeg(head);
+	printDLL(head);
+	
+}
