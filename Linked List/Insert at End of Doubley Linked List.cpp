8163953f93dd66a39844445a24da84ade0448fcc
Aditya Raj <65248775+themlstudent@users.noreply.github.com>
2021-04-25 12:23:43 +0530

    Add files via upload

---
+++ Linked List/Insert at End of Doubley Linked List.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+class Node
+{
+     public:
+	 int data;
+	 Node *prev;
+	 Node *next;
+	 Node(int x)
+	 {
+	    data=x;
+		prev=NULL;
+		next=NULL;	
+	 }	
+};
+Node *insertTail(Node *head, int data)
+{
+	Node *temp=new Node(data);
+	if(head==NULL) return temp;
+	Node*curr=head;
+	while(curr->next!=NULL)
+	{
+		curr=curr->next;
+     }
+     curr->next=temp;
+     temp->prev=curr;
+     return head;
+}
+void printDLL(Node *head)
+{
+	Node *curr=head;
+	while(curr!=NULL)
+	{
+		cout<<curr->data<<"---->";
+		curr=curr->next;
+	}
+	cout<<endl;
+}
+int main()
+{
+	Node* head=new Node(10);
+	Node*temp1=new Node(20);
+	Node *temp2=new Node(30);
+	head->next=temp1;
+	temp1->prev=head;
+	temp1->next=temp2;
+	temp2->prev=temp1;
+	printDLL(head);
+	head=insertTail(head,40);
+	printDLL(head);
+	
+}
