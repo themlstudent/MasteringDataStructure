af118470126ced880cc36c19d60d24015f96d8be
Aditya Raj <65248775+themlstudent@users.noreply.github.com>
2021-04-27 20:54:40 +0530

    Add files via upload

---
+++ Linked List/Insert at End of Circular Linked LisEfficient)t.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+     public:
+	 int data;
+	 Node *next;
+	 Node(int x)
+	 {
+	 	data=x;
+	 	next=NULL;
+		 }	
+};
+void printlist(Node *head)
+{
+	if(head==NULL) return ;
+	cout<<head->data<<"-->";
+	for(Node*p=head->next;p!=head;p=p->next)
+	{
+		cout<<p->data<<"--->";
+	}
+}
+Node *insertEnd(Node *head,int d)
+{
+	Node*temp=new Node(d);
+	if(head==NULL)
+	{
+		temp->next=temp;
+		return temp;
+	}	
+	else{
+		temp->next=head->next;
+		head->next=temp;
+		int t=head->data;
+		head->data=temp->data;
+		temp->data=t;
+		return temp;
+	}
+	
+}
+int main()
+{
+	Node *head=new Node(10);
+	head->next=new Node(5);
+	head->next->next=new Node(20);
+	head->next->next->next=new Node(15);
+	head->next->next->next->next=head;
+	printlist(head);
+	cout<<endl;
+	cout<<"After Insertion"<<endl;
+	head=insertEnd(head,25);
+	
+	printlist(head);
+}
